627b5ed6db9732c9cf7cbe10b449fb343bd02f99
Nikhil Metrani <nikhilmetrani@gmail.com>
2015-11-21 12:49:34 +0800

    Initial Commit

---
+++ Quake Catalogue/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Quake Catalogue
+//
+//  Created by Nikhil Metrani on 21/11/15.
+//  Copyright © 2015 Group04. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
